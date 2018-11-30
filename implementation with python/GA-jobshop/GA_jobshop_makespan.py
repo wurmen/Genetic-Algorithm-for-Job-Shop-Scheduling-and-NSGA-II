7cf34725f5ad6d99016b8620703de348bd2038d4
Cheng-Man Wu <ftcu5931@gmail.com>
2018-11-30 16:08:11 +0800

    Update GA_jobshop_makespan.py

--- implementation with python/GA-jobshop/GA_jobshop_makespan.py
+++ implementation with python/GA-jobshop/GA_jobshop_makespan.py
@@ -191,3 +191,43 @@ plt.plot([i for i in range(len(makespan_record))],makespan_record,'b')
 plt.ylabel('makespan',fontsize=15)
 plt.xlabel('generation',fontsize=15)
 plt.show()
+
+
+'''--------plot gantt chart-------'''
+import pandas as pd
+import plotly.plotly as py
+import plotly.figure_factory as ff
+import datetime
+
+m_keys=[j+1 for j in range(num_mc)]
+j_keys=[j for j in range(num_job)]
+key_count={key:0 for key in j_keys}
+j_count={key:0 for key in j_keys}
+m_count={key:0 for key in m_keys}
+j_record={}
+for i in sequence_best:
+    gen_t=int(pt[i][key_count[i]])
+    gen_m=int(ms[i][key_count[i]])
+    j_count[i]=j_count[i]+gen_t
+    m_count[gen_m]=m_count[gen_m]+gen_t
+    
+    if m_count[gen_m]<j_count[i]:
+        m_count[gen_m]=j_count[i]
+    elif m_count[gen_m]>j_count[i]:
+        j_count[i]=m_count[gen_m]
+    
+    start_time=str(datetime.timedelta(seconds=j_count[i]-pt[i][key_count[i]])) # convert seconds to hours, minutes and seconds
+    end_time=str(datetime.timedelta(seconds=j_count[i]))
+        
+    j_record[(i,gen_m)]=[start_time,end_time]
+    
+    key_count[i]=key_count[i]+1
+        
+
+df=[]
+for m in m_keys:
+    for j in j_keys:
+        df.append(dict(Task='Machine %s'%(m), Start='2018-07-14 %s'%(str(j_record[(j,m)][0])), Finish='2018-07-14 %s'%(str(j_record[(j,m)][1])),Resource='Job %s'%(j+1)))
+    
+fig = ff.create_gantt(df, index_col='Resource', show_colorbar=True, group_tasks=True, showgrid_x=True, title='Job shop Schedule')
+py.iplot(fig, filename='GA_job_shop_scheduling', world_readable=True)
